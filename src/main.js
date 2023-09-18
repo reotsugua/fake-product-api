975ee7f26341bac97363ba72c2c6efbca216ec7c
pablo <pablords@gmail.com>
2023-09-18 10:10:25 -0300

    feat: added api routes for products

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import express from "express"
+import cors from "cors"
+import data from "../products.json" assert { type: 'json' };
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+
+function paginate(array, pageSize, pageNumber) {
+    return array.slice((pageNumber - 1) * pageSize, pageNumber * pageSize);
+}
+
+
+app.get("/products", async (req, res) => {
+    let products = data
+    const searchText = req.query.search?.toLocaleLowerCase()
+    const productsSearchResult = data.filter(product => searchText ? product?.name?.toLowerCase().includes(searchText) : product)
+    if (req.query?.pageSize || req.query?.pageNumber) {
+        products = paginate(productsSearchResult, req.query?.pageSize, req.query?.pageNumber)
+    }
+    setTimeout(() => res.json(products), products.length * 25)
+})
+
+app.listen(3000, () => {
+    console.log("Server started at port 3000")
+})
